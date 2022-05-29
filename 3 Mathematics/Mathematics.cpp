5296cb4aacd656661bcf07e6203e5502a7802294
Oleh Zakhozhai <zakhozhay.oleg@gmail.com>
2022-05-29 23:34:40 +0300

    New projects was added

---
+++ 3 Mathematics/Mathematics.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <windows.h>
+#include <conio.h>
+#include <string>
+#include <math.h> // Підключення маьематичної бібліотеки для доступу до математичних функцій
+#pragma hdrstop
+#pragma argsused
+
+#ifdef _WIN32
+#include <tchar.h>
+#else
+  typedef char _TCHAR;
+  #define _tmain main
+#endif
+
+#include <stdio.h>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	SetConsoleOutputCP(1251);
+
+	int x, y, z;
+	std::cout<<"Введіть аргументи x y z: ";
+	std::cin>>x>>y>>z;
+	double result = (45-x)/34+x/(pow(y,3)-z)+sin(x+z); //Рядковий запиз математичного вираза з використанням математичних фуекцій
+	std::cout<<"Результат розрахунку дорівнює: "<<result;
+	
+	getch();
+	return 0;
+}
